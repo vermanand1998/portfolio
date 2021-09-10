0557e57283a920a0b2880a8ff982a1cdcb5a486b
Anand VErma <67152953+vermanand1998@users.noreply.github.com>
2021-09-10 11:25:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["web Developer","Java Developer","Freelancer"],
+        strings: ["Web Developer","Java Developer","Project Engineer","React Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["web Developer","Java Developer","Freelancer"],
+        strings: ["Web Developer","Java Developer","Project Engineer","React Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
